cceeea93b8f6c7007eed83cc701679772dd53f6e
Steven <stevenglab@gmail.com>
2024-12-13 16:26:19 -0700

    docker compose gets up

--- app/__init__.py
+++ app/__init__.py
@@ -4,11 +4,11 @@ from flask_cors import CORS
 from flask_migrate import Migrate
 from flask_wtf.csrf import CSRFProtect, generate_csrf
 from flask_login import LoginManager
-from .models import db, User
-from .api.user_routes import user_routes
-from .api.auth_routes import auth_routes
-from .seeds import seed_commands
-from .config import Config
+from models import db, User
+from api.user_routes import user_routes  # Remove the .
+from api.auth_routes import auth_routes  # Remove the .
+from seeds import seed_commands  # Remove the .
+from config import Config  # Remove the .
 
 app = Flask(__name__)
 
