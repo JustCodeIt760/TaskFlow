28ac36bf856ff1d5d7336523eb14f3db83895aa2
justcodeit760 <jakemann.com@gmail.com>
2024-12-14 10:18:55 -0800

    add deploy script

--- react-vite/src/router/index.jsx
+++ react-vite/src/router/index.jsx
@@ -9,7 +9,7 @@ export const router = createBrowserRouter([
     children: [
       {
         path: "/",
-        element: <h1>Welcome!</h1>,
+        element: <h1>Just code it</h1>,
       },
       {
         path: "login",
