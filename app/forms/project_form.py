3e6d2cbba300403eca54d94b042bf47b29e2dda6
selin <uzunogluselin92@gmail.com>
2024-12-17 18:21:07 -0600

    added length validator to description

--- app/forms/project_form.py
+++ app/forms/project_form.py
@@ -8,7 +8,10 @@ class ProjectForm(FlaskForm):
         DataRequired(), 
         Length(min=1, max=100, message='Name must be between 1 and 100 characters')
     ])
-    description = TextAreaField('description')
+    description = TextAreaField('description', 
+    DataRequired(),
+    validators=[Length(min=10, max=300, message='Description must be less than 300 characters')
+    ])
     due_date = DateField('due_date', validators=[DataRequired()])
 
     def validate_due_date(self, field):
