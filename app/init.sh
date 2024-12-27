b46518340edbdb7acf8d8e096133a646fad88556
Steven <stevenglab@gmail.com>
2024-12-26 17:05:13 -0700

    added ignore of init.sh to gitignore

--- app/init.sh
+++ app/init.sh
@@ -14,20 +14,20 @@ done
 echo "PostgreSQL started successfully!"
 
 # Clean start - remove old migrations
-echo "Cleaning old migrations..."
-rm -rf migrations/
-flask db init
+#echo "Cleaning old migrations..."
+#rm -rf migrations/
+#flask db init
 
 # Create and run migrations
-echo "Creating and running migrations..."
-flask db migrate -m "initial migration"
-flask db upgrade
-echo "Migrations completed!"
+#echo "Creating and running migrations..."
+#flask db migrate -m "initial migration"
+#flask db upgrade
+#echo "Migrations completed!"
 
 # Seeds (no need to undo since we just recreated tables)
-echo "Running seeds..."
-flask seed all
-echo "Seeds completed!"
+#echo "Running seeds..."
+#flask seed all
+#echo "Seeds completed!"
 
 echo "Starting Gunicorn server..."
 exec gunicorn --bind 0.0.0.0:5000 "__init__:app"
