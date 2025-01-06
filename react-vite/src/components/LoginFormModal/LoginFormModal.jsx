6cb0170488876b954f663bbaf8dfb7909f78af0a
selin <uzunogluselin92@gmail.com>
2025-01-05 19:13:00 -0600

    added css to demo user

--- react-vite/src/components/LoginFormModal/LoginFormModal.jsx
+++ react-vite/src/components/LoginFormModal/LoginFormModal.jsx
@@ -1,105 +1,93 @@
-import { useState } from "react";
-import { useDispatch } from "react-redux";
-import { useModal } from "../../context/Modal";
-import { thunkLogin } from "../../redux/session";
-import { refreshAllData } from "../../redux/shared";
-import styles from "./LoginForm.module.css";
+import { useEffect, useState } from 'react';
+import { setErrors, thunkLogin, selectErrors } from '../../redux/session';
+import { useDispatch, useSelector } from 'react-redux';
+import { useModal } from '../../context/Modal';
+import './LoginForm.css';
+import { thunkLoadProjects } from '../../redux/project';
+import { thunkLoadTasks } from '../../redux/task';
+import { thunkLoadFeatures } from '../../redux/feature';
+import { thunkLoadSprints } from '../../redux/sprint';
 
 function LoginFormModal() {
   const dispatch = useDispatch();
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const errors = useSelector(selectErrors);
   const { closeModal } = useModal();
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [errors, setErrors] = useState({});
+
+  useEffect(() => {
+    return () => {
+      dispatch(setErrors(null));
+    };
+  }, [dispatch]);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    try {
-      const serverResponse = await dispatch(
-        thunkLogin({
-          email,
-          password,
-        })
-      );
 
-      if (serverResponse?.errors) {
-        setErrors(serverResponse.errors);
-      } else {
-        await dispatch(refreshAllData());
-        closeModal();
-      }
-    } catch (error) {
-      console.error("Login failed:", error);
+    const serverResponse = await dispatch(
+      thunkLogin({
+        email,
+        password,
+      })
+    );
+
+    if (serverResponse) {
+      dispatch(thunkLoadProjects());
+      dispatch(thunkLoadTasks());
+      dispatch(thunkLoadFeatures());
+      dispatch(thunkLoadSprints());
+      closeModal();
     }
   };
 
   const handleDemoLogin = async (e) => {
     e.preventDefault();
-    try {
-      const serverResponse = await dispatch(
-        thunkLogin({
-          email: "demo@aa.io",
-          password: "password"
-        })
-      );
+    const serverResponse = await dispatch(
+      thunkLogin({
+        email: "demo@aa.io",
+        password: "password"
+      })
+    );
 
-      if (serverResponse?.errors) {
-        setErrors(serverResponse.errors);
-      } else {
-        await dispatch(refreshAllData());
-        closeModal();
-      }
-    } catch (error) {
-      console.error("Demo login failed:", error);
-      setErrors({ general: "Failed to log in with demo account" });
+    if (serverResponse) {
+      dispatch(thunkLoadProjects());
+      dispatch(thunkLoadTasks());
+      dispatch(thunkLoadFeatures());
+      dispatch(thunkLoadSprints());
+      closeModal();
     }
   };
 
   return (
-    <div className={styles.loginFormContainer}>
+    <>
       <h1>Log In</h1>
-      {errors.general && (
-        <p className={styles.error}>{errors.general}</p>
-      )}
       <form onSubmit={handleSubmit}>
-        <div className={styles.formGroup}>
-          <label>
-            Email
-            <input
-              type="text"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              required
-            />
-          </label>
-          {errors.email && <p className={styles.error}>{errors.email}</p>}
-        </div>
-
-        <div className={styles.formGroup}>
-          <label>
-            Password
-            <input
-              type="password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              required
-            />
-          </label>
-          {errors.password && <p className={styles.error}>{errors.password}</p>}
-        </div>
-
-        <div className={styles.buttonGroup}>
-          <button type="submit" className={styles.loginButton}>Log In</button>
-          <button 
-            type="button" 
-            onClick={handleDemoLogin}
-            className={styles.demoButton}
-          >
-            Demo User Login
-          </button>
-        </div>
+        <label>
+          Email
+          <input
+            type="text"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
+        </label>
+        {errors?.email && <p>{errors.email}</p>}
+        <label>
+          Password
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+        </label>
+        {errors?.password && <p>{errors.password}</p>}
+        <button type="submit">Log In</button>
+        <button type="demo-button" onClick={handleDemoLogin}>
+          Demo User Login
+        </button>
       </form>
-    </div>
+    </>
   );
 }
 
